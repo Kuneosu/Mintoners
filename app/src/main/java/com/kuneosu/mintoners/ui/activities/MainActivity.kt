8c61673e9f5ab9a4ca340876aba0df1e3d70d391
Kuneosu <kks92155784@gmail.com>
2024-07-08 15:06:02 +0900

    프로필 화면 추가, 네비게이션 연결, 모든 프래그먼트 최상단 뷰를 스크롤뷰로 변경

--- app/src/main/java/com/kuneosu/mintoners/ui/activities/MainActivity.kt
+++ app/src/main/java/com/kuneosu/mintoners/ui/activities/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.kuneosu.mintoners.ui.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.kuneosu.mintoners.R
 import com.kuneosu.mintoners.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,8 +21,26 @@ class MainActivity : AppCompatActivity() {
             R.layout.activity_main
         )
 
+
+
         binding.mainBottomNav.background = null
         binding.mainBottomNav.menu.getItem(1).isEnabled = false
 
+        binding.mainFloatingAddBtn.setOnClickListener {
+            Toast.makeText(this, "Add Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.mainBottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> {
+                    binding.mainContainer.findNavController().navigate(R.id.action_profileMainFragment_to_homeFragment)
+                }
+                R.id.menu_profile -> {
+                    binding.mainContainer.findNavController().navigate(R.id.action_homeFragment_to_profileMainFragment)
+                }
+            }
+            true
+        }
     }
+
 }
---
+++ app/src/main/java/com/kuneosu/mintoners/ui/fragments/ProfileMainFragment.kt
@@ -0,0 +1,33 @@
+package com.kuneosu.mintoners.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kuneosu.mintoners.R
+import com.kuneosu.mintoners.databinding.FragmentProfileMainBinding
+
+
+class ProfileMainFragment : Fragment() {
+
+    private var _binding: FragmentProfileMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
