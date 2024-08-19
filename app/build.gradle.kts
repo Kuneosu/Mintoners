ec5fe077d1e1e6ee80ec8ccf7057ad7b293efc70
Kuneosu <kks92155784@gmail.com>
2024-08-19 09:33:48 +0900

    Mintoners 정식 출시, 버전 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.kuneosu.mintoners"
         minSdk = 24
         targetSdk = 34
-        versionCode = 23
-        versionName = "1.0.6"
+        versionCode = 240819
+        versionName = "1.0.7"
 
         val properties = Properties()
         properties.load(project.rootProject.file("local.properties").inputStream())
